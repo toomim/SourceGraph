d8fc370b0027d414359872cfcb48bdd8e5258721
steven elleman <steven.elleman@scaleft.com>
2018-10-23 09:28:49 -0700

    * Basic metadata header

--- frontend/src/components/WritingPanel.jsx
+++ frontend/src/components/WritingPanel.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 export class WritingPanel extends React.Component {
+  /*
+  Goal: The Writing Panel should be responsible for making new sources. There will be three main components of the Panel: (1) TextArea (2) Publish button and (3) Metadata Header
+
+  The Metadata Header will consist of:
+  1. title
+  2. creator(s)
+  3. creation-timestamp (defaults to the publish time)
+  4. publish-timestamp
+  5. tags
+   */
+
   constructor(props) {
     super(props);
     this.state = {};
@@ -15,6 +26,7 @@ export class WritingPanel extends React.Component {
 
   render () {
     const margin = 5;
+    const headerStyle ={}
     const colStyle = {
         outline: '5px dotted black',
         height: '100%',
@@ -37,6 +49,20 @@ export class WritingPanel extends React.Component {
     };
 
     return <div style={colStyle}>
+      <div style={headerStyle}>
+        <label>
+          Title:
+          <input type="text" />
+        </label>
+        <label>
+          Creator(s):
+          <input type="text" />
+        </label>
+        <label>
+          Creation Time:
+          <input type="text" />
+        </label>
+      </div>
       <textarea ref='source' style={inputStyle} type="text" name="name" defaultValue={this.state.source}/>
       <button style={buttonStyle} onClick={this.updateSource.bind(this)}> Publish </button>
     </div>;
